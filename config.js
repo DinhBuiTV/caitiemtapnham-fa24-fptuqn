370302f03850adcdf0e41413a7a7c14dbb4f6a44
DinhBuiTV <nguyendinhct2004@gmail.com>
2024-09-26 22:22:48 +0700

    JS & HTML
    JS & HTML

---
+++ config.js
@@ -0,0 +1,15 @@
+// Preloader Conditions
+
+window.addEventListener("load", function () {
+    const preloader = document.querySelector(".preloader");
+    preloader.className += " conditions";
+  
+    const nav_bar = document.querySelector("nav.navigation-bar-menu");
+    nav_bar.className += " conditions";
+  
+    const header_information = document.querySelector(".header-information");
+    header_information.className += " conditions";
+  
+    const header_image_title = document.querySelector(".header-slogan-logo");
+    header_image_title.className += " conditions";
+  });
