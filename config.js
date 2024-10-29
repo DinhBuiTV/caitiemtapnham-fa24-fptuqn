fd8be98a8561c7e44c9df22d2207088a75a254fb
DinhBuiTV <nguyendinhct2004@gmail.com>
2024-10-29 17:13:29 +0700

    Change

--- config.js
+++ config.js
@@ -13,3 +13,21 @@ window.addEventListener("load", function () {
     const header_image_title = document.querySelector(".header-slogan-logo");
     header_image_title.className += " conditions";
   });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    var carousel = document.getElementById('carouselExampleInterval');
+    var images = carousel.querySelectorAll('.carousel-item img');
+
+    carousel.addEventListener('slid.bs.carousel', function () {
+        // Remove the animation class from all images
+        images.forEach(function (img) {
+            img.classList.remove('animate-slide');
+        });
+
+        // Add the animation class to the active image
+        var activeImage = carousel.querySelector('.carousel-item.active img');
+        if (activeImage) {
+            activeImage.classList.add('animate-slide');
+        }
+    });
+});
